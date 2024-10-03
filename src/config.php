6ea5b733a1d3cd1adc51dff1af5887669573d128
STougeiro <sidneytougeiro@gmail.com>
2024-10-03 08:33:41 -0300

    View config file example

---
+++ src/config.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+    return
+    [
+        'storage_separator' => ':',
+
+        'file_extension' => '.html',
+
+        'temporary_directory' => sys_get_temp_dir() . DIRECTORY_SEPARATOR,
+    ];
